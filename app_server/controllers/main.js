82138db58bc3d57574e62bba17e2f46de28a2c92
Aryan <aryanaligetibusiness@gmail.com>
2024-07-13 11:21:23 -0700

    first commit

---
+++ app_server/controllers/main.js
@@ -0,0 +1,8 @@
+/* GET homepage */
+const index = (req, res) => {
+    res.render('index', { title: 'Car Rental System' });
+    };
+    module.exports = {
+        index
+        };
+                                        
